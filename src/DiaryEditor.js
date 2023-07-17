01d91bafb06e49de2beb4d56c342415ff5bb657a
softmoca <acyc0909@gmail.com>
2023-07-17 20:46:26 +0900

    feat: uesRef foucs

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,6 +18,16 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
     console.log(state);
     alert("저장 성공!");
   };
@@ -24,6 +37,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           value={state.author}
           onChange={handleChangeState}
           name="author"
@@ -33,6 +47,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           value={state.content}
           onChange={handleChangeState}
           name="content"
