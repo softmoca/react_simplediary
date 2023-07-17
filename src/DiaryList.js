7d2c703a8bbc9de3e014ed14d4529ad7cd840adc
softmoca <acyc0909@gmail.com>
2023-07-17 23:04:09 +0900

    feat:ueseffect

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,21 +1,76 @@
-import DiaryItem from "./DiaryItem";
+import { useRef, useState } from "react";
 
-const DiaryList = ({ onEdit, onRemove, diaryList }) => {
+const DiaryItem = ({
+  onRemove,
+  onEdit,
+  id,
+  author,
+  content,
+  emotion,
+  created_date,
+}) => {
+  const localContentInput = useRef();
+  const [localContent, setLocalContent] = useState(content);
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const handleClickRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
   return (
-    <div className="DiaryList">
-      <h2>일기 리스트</h2>
-      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
-      <div>
-        {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
-        ))}
+    <div className="DiaryItem">
+      <div className="info">
+        <span className="author_info">
+          작성자 : {author} | 감정 : {emotion}
+        </span>
+        <br />
+        <span className="date">
+          {new Date(created_date).toLocaleDateString()}
+        </span>
       </div>
+      <div className="content">
+        {isEdit ? (
+          <textarea
+            ref={localContentInput}
+            value={localContent}
+            onChange={(e) => setLocalContent(e.target.value)}
+          />
+        ) : (
+          content
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleClickRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
-
-DiaryList.defaultProps = {
-  diaryList: [],
-};
-
-export default DiaryList;
+export default DiaryItem;
