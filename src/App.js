461d42904bc84abe0a55f1cf8b5c28c0f94d73b4
softmoca <acyc0909@gmail.com>
2023-07-17 21:37:12 +0900

    feat: add data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-const dummyList = [
-  {
-    id: 1,
-    author: "이정환",
-    content: "하이1",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "이정환",
-    content: "하이2",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "이정환",
-    content: "하이3",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
-function App() {
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <DiaryEditor />
-        <DiaryList diaryList={dummyList} />
-      </header>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
-}
-
+};
 export default App;
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from "react";
+import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -28,8 +28,13 @@ const DiaryEditor = () => {
       return;
     }
 
-    console.log(state);
-    alert("저장 성공!");
+    onCreate(state.author, state.content, state.emotion);
+    alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   };
 
   return (
